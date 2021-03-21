f84c5efb9109370a910864f05622d0ae1436fc1b
znurtokatli <50844314+znurtokatli@users.noreply.github.com>
2021-03-21 15:49:06 -0400

    srcadded to main

---
+++ src/config.py
@@ -0,0 +1,4 @@
+db_user = "postgres"
+db_pwd = "welcome1"
+db_name = "ohpydb"  
+conn_str = "postgres:welcome1@localhost:5432/ohpydb"
--- src/db-objects.sql
+++ src/db-objects.sql
@@ -1,36 +1,52 @@
+create database ohpydb
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8';
+ 
+--create tables under ohpydb schema 
+
 create table award_categories(
 	category_id serial not null primary key,
 	category_name varchar(100) not null,
-	subcategory_name varchar(100),
-	description varchar(200),
+	subcategory_name varchar(100) 
 	constraint award_categories_uk unique (category_name, subcategory_name)
 );
  
+create table nominees(
+	nominee_id serial not null primary key ,
+	nominee_name varchar(100) not null 
+);
+     
 create table films(
 	film_id serial not null primary key ,
 	film_name varchar(100) not null,
-	release_date date
+	release_date date,
+	nominee_id int,  --for director names
+	imdb_score int ,
+	rotten_tomatoes_score int,
+	service1 boolean, --Netflix			
+	service2 boolean, --Hulu
+	service3 boolean, --Prime Video
+	service4 boolean, --Disney+
+	first_genre varchar2(100),
+	second_genre varchar2(100),
+	third_genre varchar2(100),
+	fourth_genre varchar2(100),
+	fifth_genre varchar2(100),
+	sixth_genre varchar2(100),
+	seventh_genre varchar2(100),
+	eigth_genre varchar2(100)
+    --constraint film_fk1 foreign key(nominee_id) references nominees(nominee_id) 
 );
-
-create table nominees(
-	nominee_id serial not null primary key ,
-	first_name varchar(100) not null,
-	last_name varchar(100),
-	date_of_birth date,
-	description varchar(100)
-);
-
-drop table academy_awards
   
 create table academy_awards(
  year varchar(5) not null,
  category_id int not null,
  film_id int, --not null,
  nominee_id int,
- winner boolean default 'N',
- description varchar(100),
+ winner boolean default 'N', 
  constraint awards_fk1 foreign key(category_id) references award_categories(category_id),
  constraint awards_fk2 foreign key(film_id) references films(film_id),	 
  constraint awards_fk3 foreign key(nominee_id) references nominees(nominee_id)	
 );
- 
+  
